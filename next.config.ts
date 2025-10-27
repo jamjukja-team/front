55d21b5b9e06b01a479c1a502feaed313e87e122
peppone-choi <peppone.choi@gmail.com>
2025-10-27 11:25:23 +0900

    feat: Axios 인스턴스와 폴더 구조 생성

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,31 @@
 import type { NextConfig } from "next";
 
+const getApiUrl = () => {
+  // 우선순위: NEXT_PUBLIC_API_URL > API_URL > DEPLOYMENT_ENV > 기본값
+  if (process.env.NEXT_PUBLIC_API_URL) {
+    return process.env.NEXT_PUBLIC_API_URL;
+  }
+  if (process.env.API_URL) {
+    return process.env.API_URL;
+  }
+}
+
+const API_URL = getApiUrl();
+
 const nextConfig: NextConfig = {
   /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: "/api/v1/:path*",
+        destination: `${API_URL}/api/v1/:path*`,
+      },
+      {
+        source: "/admin-api/:path*",
+        destination: `${API_URL}/admin-api/:path*`,
+      },
+    ]
+  }
 };
 
 export default nextConfig;
